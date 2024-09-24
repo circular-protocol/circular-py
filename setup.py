687f8a51557496333def4dde803718f42a1721f9
Danny De Novi <70696078+dannydenovi@users.noreply.github.com>
2024-09-24 18:38:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='circular_protocol_api',
-    version='1.0.88',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'requests',
